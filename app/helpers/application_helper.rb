3238aea44513f2df26067e2bd09d31b7a21f4f34
s33dco <code@s33d.co>
2018-06-21 22:33:35 +0100

    add set up for rspec and factory bot

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+
+# for meta page titles 
+
+  def title(title)
+  	content_for(:title, title)
+  end
+
+	def page_title
+  	if content_for?(:title)
+  		"#{content_for(:title)} | #{ENV['COOP_SHORT_NAME']}"
+  	else
+  		"#{ENV['COOP_NAME']}"
+  	end
+  end
+
+ # for meta descriptions
+
+  def description(description)
+  	content_for(:description, description)
+  end
+
+	def page_description
+  	if content_for?(:description)
+  		"#{content_for(:description)}"
+  	else
+  		"#{ENV['COOP_DESCRIPTION']}"
+  	end
+  end
+
 end
