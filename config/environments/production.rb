48c16254001c7e3ec3cfb80851d264ae9431e06d
s33dco <code@s33d.co>
2018-07-19 23:10:58 +0100

    set up for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,23 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'immense-thicket-90474.herokuapp.com.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    domain:               'heroku.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true 
+  }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
