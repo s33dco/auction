8c643f35646ff37702c93d4d6994f5345aca77eb
s33dco <code@s33d.co>
2018-07-19 22:01:09 +0100

    prepare for heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,6 +19,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.secret_key = '7207419ebaf475f9af48ff3a43ba3545ff5ecb0ef1447510db5da78b52467c0497d56de70a56546d083f4a5fd9f8b54f04a145c99ac05fda79744444abfaf0e7'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
