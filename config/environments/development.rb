76227ae489cf721cfd51b3e93ee01170ccd58de4
s33dco <code@s33d.co>
2018-08-02 01:33:35 +0100

    resize large images to 200px small to 100px

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+ 
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -32,9 +33,6 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-
-
-
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: "localhost", port:3000 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'heroku.com',
+    domain:               'exsale.co.uk',
     user_name:            ENV["SENDGRID_USERNAME"],
     password:             ENV["SENDGRID_PASSWORD"],
     authentication:       'plain',
