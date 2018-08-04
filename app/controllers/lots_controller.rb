8e6048d0572156459e25b1369324da428bd03dfb
s33dco <code@s33d.co>
2018-08-04 01:12:42 +0100

    go with render edit

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -56,7 +56,7 @@ class LotsController < ApplicationController
 	def delete_one_attached_picture
 		@lot = Lot.find(params[:id])
 		@lot.pictures.find(params[:image_id]).purge
-		redirect_to edit_lot_url(@lot.id), alert: "Image deleted"
+		render :edit, alert: "Image deleted"
 	end
 
 	def delete_main_image
