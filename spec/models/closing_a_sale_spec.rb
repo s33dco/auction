2e094b14d05f4de56a987479870222f331ae3c27
s33dco <code@s33d.co>
2018-09-04 23:53:45 +0100

    flesh out specs for bidding and sellingprice during auction sale

---
+++ spec/models/closing_a_sale_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "closing a sale" do
+
+	it 'marks sale as complete'
+
+	it 'only sells if above reserve price'
+
+	it 'identifies the highest bid marking won as true'
+
+	it 'identifies the highest bidder'
+
+	it 'sells at the second highest bid plus notch'
+
+	it 'marks as sold'
+
+	it 'marks as buyer not paid'
+
+	it 'marks as seller not paid'
+
+	it 'it calculates the correct selling fee when minimum fee applies'
+
+	it 'it calculates the correct selling fee when precentage fee applies'
+
+	it 'it calculates the correct buying fee when minimum fee applies'
+
+	it 'it calculates the correct buying fee when precentage fee applies'
+
+end
---
+++ spec/models/opening_a_sale_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "opening a sale" do
+
+	it 'will open a sale if one or more lots'
+
+	it 'marks sale as active'
+
+	it 'show a sale as not complete'
+
+end
